b6690957fda01c66fc5ebf066b700e45a74f3439
IndigoViolet <agustinus.nabiel@gmail.com>
2022-08-15 14:27:56 +0100

    Console.log entire translation instead of iteratin

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import "./App.css";
 
 function App() {
   async function fetchStuffHanlder() {
-    const response = await fetch("https://api.quran.com/api/v4/chapters?language=en");
-    const data = await response;
-    // console.log("Test")
-    console.log(data.json());
+    const response = await fetch("https://api.quran.com/api/v4/verses/random?language=en&translations=131");
+    const data = await response.json();
+
+    console.log("Test")
+
+    // for (let i in data.words){
+    //   console.log(data.verse.words[i].translation.te xt);
+    // }
+
+    // data.verse.words.forEach((dat) => {
+    //   console.log(dat.translation.text);
+    // });
+
+    console.log(data.verse.translations[0].text);
   }
 
   return (

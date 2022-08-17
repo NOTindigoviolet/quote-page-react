d6b8ada9f7148f7edc32c3e0707448b9d9d0cf63
IndigoViolet <agustinus.nabiel@gmail.com>
2022-08-17 17:21:01 +0100

    Calls API on refresh

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  async function fetchEnglishHandler() {
-    const response = await fetch("https://api.quran.com/api/v4/verses/random?language=en&translations=131");
-    const data = await response.json();
+  const fetchEnglishHandler = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "https://api.quran.com/api/v4/verses/random?language=en&translations=131"
+      );
+      const data = await response.json();
 
-    console.log("Test")
+      console.log("Test");
 
-    const num = data.verse.verse_key
+      const num = data.verse.verse_key;
 
-    const uri = 'https://api.quran.com/api/v4/quran/verses/uthmani?verse_key=' + encodeURIComponent(num);
+      const uri =
+        "https://api.quran.com/api/v4/quran/verses/uthmani?verse_key=" +
+        encodeURIComponent(num);
 
-    const response2 = await fetch(uri);
-    const data2 = await response2.json()
+      const response2 = await fetch(uri);
+      const data2 = await response2.json();
 
-    // for (let i in data.words){
-    //   console.log(data.verse.words[i].translation.te xt);
-    // }
+      console.log(data.verse.translations[0].text);
 
-    // next try to get full arabic text using ayah number 
+      console.log(data2.verses[0].text_uthmani);
 
-    // data.verse.words.forEach((dat) => {
-    //   console.log(dat.translation.text);
-    // });
+      console.log(num);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
-    console.log(data.verse.translations[0].text);
-
-    console.log(data2.verses[0].text_uthmani);
-
-    console.log(num);
-  }
+  useEffect(() => {
+    fetchEnglishHandler();
+  }, [fetchEnglishHandler]);
 
   return (
     <React.Fragment>
-      <button onClick={fetchEnglishHandler}>Get English</button>
       <header className="App-header">
         <p>HELLO</p>
       </header>

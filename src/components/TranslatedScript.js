b5c61c70a29bea8f795ebb480abaaa42a3701309
IndigoViolet <agustinus.nabiel@gmail.com>
2022-08-29 16:52:15 +0100

    Made some quick css, not finalised

--- src/components/TranslatedScript.js
+++ src/components/TranslatedScript.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import classes from './TranslatedScript.module.css';
+
 function TranslatedScript(props) {
   return (
-    <section>
+    <section className={classes.script}>
       <h1>{props.text.arb}</h1>
       <h3>{props.text.eng}</h3>
       <h3>{props.text.verse}</h3>

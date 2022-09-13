a16844aee3cecf12da1143ba48d0912413b85718
IndigoViolet <agustinus.nabiel@gmail.com>
2022-09-13 23:06:51 +0100

    Saving work for remote

--- src/components/VerseForm.js
+++ src/components/VerseForm.js
@@ -1,18 +1,34 @@
 import React from "react";
 
 function VerseForm(props) {
+  async function fetchVerseHandler(event) {
+    event.preventDefault();
+    console.log(event.target.value);
+    // try {
+    //   const uri =
+    //     "https://api.quran.com/api/v4/quran/verses/uthmani?verse_key=" +
+    //     encodeURIComponent(surah);
+
+    //   const response2 = await fetch(uri);
+    //   const data2 = await response2.json();
+    //   console.log(data2);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  }
+
   return (
     <form>
       <div className="form-main">
         <label htmlFor="surah">Enter Surah</label>
-        <input type="number" id="surah" />
+        <input type="text" id="surah" name="surah"/>
       </div>
       <div className="form-main">
         <label htmlFor="ayah">Enter Ayah</label>
-        <input type="number" id="ayah" />
+        <input type="text" id="ayah" name="ayah"/>
       </div>
       <div>
-        <button>Submit</button>
+        <button onClick={fetchVerseHandler} type="submit">Submit</button>
       </div>
     </form>
   );
